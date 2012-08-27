3d63b22595b44de7dda0e56d2405d3acd217201c
Janus <ysangkok@gmail.com>
2012-08-27 03:02:18 +0200

    everything should work now

--- arguments.js
+++ arguments.js
@@ -1,3 +1,5 @@
 var Module = {};
 //Module["arguments"] = ["1", "1087", "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"];
 Module["arguments"] = "1 0 5 0 -1".split(" ");
+Module["noExitRuntime"] = true;
+Module["print"] = function(x) { parent.stdoutLine(x); };
--- war/janus_frame.php
+++ war/janus_frame.php
@@ -1,25 +1,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <script>
-
+function setEnable(b) {
+if (b)
+window["localStdout"] = function(line) {lineBuf.push(line); };
+else
+window["localStdout"] = function(line) {};
+}
+var stdoutLine = function(line) { lineBuf.push(line); };
 var canCall = false;
-var finished = function(result) { parent.resultDone(document.getElementById("ifrm").src, result); }
-var lineBuf = [];
-var integerMathLoaded = function() { document.getElementById("ifrm").contentWindow.initRuntime(); console.log("Can call!"); canCall = true; };
+var finished = function(result) { console.log("Finished"); parent.resultDone(document.getElementById("ifrm").src, result); }
+var lineBuf;
 var call = function(list, count) {
 if (!canCall) throw new Error("Can't call yet; not finished loading");
-document.getElementById("ifrm").contentWindow.msg(JSON.stringify(arguments));
-
+lineBuf = [];
+setEnable(true);
+console.log(JSON.stringify(arguments));
+//document.getElementById("ifrm").contentWindow.Module.print=stdoutLine;
 document.getElementById("ifrm").contentWindow.run(list);
 var pollRate = 50;
 var check = function() {
 	//alert(lineBuf.length);
 	if (lineBuf.length < count) setTimeout(check,pollRate);
-	else { finished(lineBuf); while(lineBuf.length!=0)lineBuf.pop(); }
+	else { setEnable(false); finished(lineBuf.splice(0,count)); }
 };
 setTimeout(check, pollRate);
 };
-var stdoutLine = function(line) { lineBuf.push(line); };
 
 </script>
 <style>
@@ -28,7 +34,7 @@ position:relative;
 height: 100%;
 }
 body {
-margin-top: 2.5em;
+margin-top: 5em;
 text-size: 1em;
 background-color:white;
 }
@@ -40,6 +46,7 @@ height:100%;
 	position: absolute;
 	top: 1em;
 	left: 1em;
+	width: 100%;
 }
 .layerc{
 position:absolute;
@@ -50,16 +57,25 @@ height:100%;
 }
 
 </style>
-
 </head>
 <body>
 <div id="container">
 <?php echo htmlentities($_GET["frame"]); ?>
+<textarea style="font-family:Terminus,monospace;font-size:10px; width:50%; float:right; " rows=4>
+window["resultDone"] = function(src, data) {
+	copy(data.join("\n"));
+	console.log(data);
+}
+</textarea>
 </div>
 <div class="layer">
 <div class="layerc">
 <iframe id="ifrm" src="<?php echo htmlentities($_GET["frame"]); ?>"></iframe>
 </div>
 </div>
+<script>
+document.getElementsByTagName('iframe')[0].onload = function() { canCall = true; };
+setEnable(false);
+</script>
 </body>
 </html>
